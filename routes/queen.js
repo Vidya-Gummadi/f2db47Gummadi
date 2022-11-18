0889871c376aee6f9bb92232ad1ce746cfda3b42
Vidya-Gummadi <gummadividyaraja1@gmail.com>
2022-11-18 09:55:19 -0600

    updated with code

--- routes/queen.js
+++ routes/queen.js
@@ -1,20 +1,20 @@
-//var express = require('express');
-//var router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-//router.get('/', function(req, res, next) {
-//  res.render('queen', { title: 'Search Results queen' });
-//});
+router.get('/', function(req, res, next) {
+  res.render('queen', { title: 'Search Results queen' });
+});
 
-//module.exports = router;
+
 var express = require('express'); 
 const queen_controlers= require('../controllers/queen'); 
 var     router = express.Router(); 
  
 /* GET costumes */ 
-//router.get('/create', queen_controlers.queen_create_Page); 
-//router.get('/update', queen_controlers.queen_update_Page); 
-//router.get('/delete', queen_controlers.queen_delete); 
+router.get('/create', queen_controlers.queen_create_Page); 
+router.get('/update', queen_controlers.queen_update_Page); 
+router.get('/delete', queen_controlers.queen_delete); 
 router.get('/detail', queen_controlers.queen_view_one_Page); 
-//router.get('/', queen_controlers.queen_view_all_Page ); 
+router.get('/', queen_controlers.queen_view_all_Page ); 
 module.exports = router; 
